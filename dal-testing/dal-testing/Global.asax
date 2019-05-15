d6ce94a07d26634fc20f24fbcd22048c47d7fddc
Billy Arredondo <22175195+gubiarpa@users.noreply.github.com>
2019-05-15 17:50:57 -0500

    Proyecto Original

---
+++ dal-testing/dal-testing/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="dal_testing.MvcApplication" Language="C#" %>
---
+++ dal-testing/dal-testing/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(dal_testing.Startup))]
+namespace dal_testing
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
